723d9c4a50753e15efd6af28e53940fa510169ab
the-last-crusaderr <52620486+the-last-crusaderr@users.noreply.github.com>
2021-01-04 19:25:36 +0530

    Add files via upload

---
+++ 018.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a num:"))
+if a < 10:
+    print("Too low")
+elif a > 10 and a < 20:
+    print("OK.")
+else:
+    print("Too high")
---
+++ 019.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a num:"))
+if a == 1:
+    print("Thank You")
+elif a == 2:
+    print("Well Done")
+elif a == 3:
+    print("Correct")
+else:
+    print("Error!!!!")
